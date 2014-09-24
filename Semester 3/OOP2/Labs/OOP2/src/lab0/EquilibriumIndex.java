3c4254cc67d3bb2f28d1552dbba4fcb6952480cb
unknown <t00152975@1XXB8CA3A80BBEA.students.ittralee.local>
2014-09-24 15:48:13 +0100

    Equilibirium Index

--- Semester 3/OOP2/Labs/OOP2/src/lab0/EquilibriumIndex.java
+++ Semester 3/OOP2/Labs/OOP2/src/lab0/EquilibriumIndex.java
@@ -1,4 +1,5 @@
 package lab0;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EquilibriumIndex {
@@ -9,29 +10,39 @@ public class EquilibriumIndex {
 		//Ask for the length of the array
 		System.out.print("How many numbers? ");
 		index = in.nextInt();
-		
+
 		int[] num = new int[index]; //Intialise the array into the amount of numbers wanted
 		//Inputting the numbers in the array
 		for (i = 0; i < num.length; i++) {
 			System.out.print("Please enter integer " + (i+1) + ": ");
 			num[i] = in.nextInt();
 		}
+		
+		System.out.println("Array is: " + Arrays.toString(num));
+		
 		//Loop through the entire array
 		for (j = 0; j < num.length; j++) {
-			//Finding the sum of the left sum
 			leftSum = 0; rightSum = 0;
-			for (k = 0; k < num.length; k++) {
-				rightSum += num[k];
-			}
-			//Finding the sum of the right sum
-			for (k = 1; k < num.length; k++) {
+			
+			//Finding the sum of the left sum
+			for (k = 0; k < j; k++) {
 				leftSum += num[k];
+				System.out.println("Leftsum is: " + leftSum);
 			}
+			
+			//Finding the sum of the right sum
+			for (k = j+1; k < num.length; k++) {
+				rightSum += num[k];
+				System.out.println("Rightsum is: " + rightSum);
+			}
+			System.out.println("Iteration: " );
+
 			//Comparing rightSum with leftSum
 			if (leftSum == rightSum)
 				System.out.print(j + " ");
-		}
 		
+		} // outer for loop
+
 		in.close();
 	}//end main
 
