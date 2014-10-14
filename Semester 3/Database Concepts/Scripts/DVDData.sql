a1e80bfefb9155cbd75d34a24c53a3c74e6fcc77
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2014-10-15 00:38:22 +0100

    Week 5: HW
    Signed-off-by: Nazmul Alam <Nazmul.Alam@students.ittralee.ie>

--- Semester 3/Database Concepts/Scripts/DVDData.sql
+++ Semester 3/Database Concepts/Scripts/DVDData.sql
@@ -10,7 +10,7 @@ INSERT INTO Age_Ratings
 VALUES('12', 'Over 12');
 
 INSERT INTO Age_Ratings
-VALUES('12A', 'Over 12, Accompanied by Adult');
+VALUES('12A', 'Over 12, Accomp by Adult');
 
 INSERT INTO Age_Ratings
 VALUES('18', 'Over 18');
@@ -60,4 +60,35 @@ INSERT INTO DVDs
 VALUES(00005, 'Seven Pound', '18', 'TS', 'OR', 'A');
 
 
+PROMPT Populating Members
+
+INSERT INTO Members
+VALUES(00001, 'Alam', 'Nazmul', 'Main Street', 'Castleisland', 'Kerry', 'danazzy@live.com', 001, 'A');
+
+INSERT INTO Members
+VALUES(00002, 'Abdul', 'Naiem', 'Main Street', 'Castleisland', 'Kerry', 'naiem@gmail.com', 000, 'I');
+
+INSERT INTO Members
+VALUES(00003, 'Bluett', 'Luke', 'Rock Avenue', 'Abbeyfeale', 'Limerick', 'ljbluet@hotmail.com', 001, 'A');
+
+INSERT INTO Members
+VALUES(00004, 'Dowling', 'Sam', 'Main Street', 'Castleisland', 'Kerry', 'samjad11@riseup.net', 001, 'A');
+
+INSERT INTO Members
+VALUES(00005, 'Landers', 'John Joe', 'Boherbee', 'Annascaul', 'Kerry', 'jjlanders@msn.com', 001, 'A');
+
+INSERT INTO Members
+VALUES(00006, 'Killian', 'Ross', 'Rock Street', 'Tralee', 'Kerry', 'ross@yahoo.ie', 001, 'A');
+
+INSERT INTO Members
+VALUES(00007, 'Sadiq', 'Samina', 'Templemore Road', 'Roscrea', 'Tipperary', 'saminas14@outlook.com', 001, 'A');
+
+INSERT INTO Members
+VALUES(00008, 'Hassan', 'Khadija', 'Clougher', 'Tralee', 'Kerry', 'khassany@live.com', 001, 'A');
+
+INSERT INTO Members
+VALUES(00009, 'Balal', 'Saba', 'Templemore Road', 'Roscrea', 'Tipperary', 'sss@gmail.com', 001, 'A');
+
+INSERT INTO Members
+VALUES(00010, 'Begum', 'Nasima', 'Main Street', 'Castleisland', 'Kerry', 'nbegum71@live.com', 001, 'A');
 
