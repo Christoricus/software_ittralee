8bd8073d41b6f017644678694577dbf09ae19224
Nazmul Alam <Nazmul.Alam@students.ittralee.ie>
2014-11-07 00:44:12 +0000

    Week 7: Thursday
    Signed-off-by: Nazmul Alam <Nazmul.Alam@students.ittralee.ie>

--- Semester 3/OOP2/Labs/OOP2/src/lab2/Library.java
+++ Semester 3/OOP2/Labs/OOP2/src/lab2/Library.java
@@ -15,24 +15,44 @@ public class Library {
 		String[] ar = {"borrowed", "rented"};
 		
 		for (int i = 0; i < ar.length; i++) {
-			day = JOptionPane.showInputDialog("Day " + ar[i]);
-			month = JOptionPane.showInputDialog("Month ") + ar[i];
-			year = JOptionPane.showInputDialog("Year " + ar[i]);
-
+			
 			if (i == 0) {
+				day = JOptionPane.showInputDialog("Day " + ar[i]);
+				month = JOptionPane.showInputDialog("Month " + ar[i]);
+				year = JOptionPane.showInputDialog("Year " + ar[i]);
+				
 				d = Integer.parseInt(day);
 				m = Integer.parseInt(month);
 				y = Integer.parseInt(year);
-				borrowDate = new GregorianCalendar(d, m, y);
+				borrowDate = new GregorianCalendar(y, m-1, d);
+			
 			} else if (i == 1) {
+				day = JOptionPane.showInputDialog("Day " + ar[i]);
 				d = Integer.parseInt(day);
+				while (returnDate.get(Calendar.DATE) < borrowDate.get(Calendar.DATE)) {
+					year = JOptionPane.showInputDialog("Error! Year " + ar[i]);
+					d = Integer.parseInt(day);
+
+				}
+				
+				month = JOptionPane.showInputDialog("Month " + ar[i]);
 				m = Integer.parseInt(month);
+				
+				year = JOptionPane.showInputDialog("Year " + ar[i]);
 				y = Integer.parseInt(year);
+				while (returnDate.get(Calendar.YEAR) != borrowDate.get(Calendar.YEAR)) {
+					year = JOptionPane.showInputDialog("Error! Year " + ar[i]);
+					y = Integer.parseInt(year);
+				}
+					
+				
 				returnDate = new GregorianCalendar(d, m, y);	
 			}
 		}
 		
 		JOptionPane.showMessageDialog(null, "Borrow date is: " + sdf.format(borrowDate.getTime()));
+		JOptionPane.showMessageDialog(null, "Borrow date is: " + sdf.format(returnDate.getTime()));
+
 		
 	}//end of main
 
